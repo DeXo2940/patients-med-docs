69bb847aedcb3951763663b40046aa5727d6cb1d
Bartosz <dexo2940@gmail.com>
2018-06-03 19:42:03 +0200

    Clever things
    Made some fancy functions

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,24 @@
 import * as React from "react";
 import "./App.css";
-import {Button} from "reactstrap";
-import {ListGroup, ListGroupItem} from "reactstrap";
-import {
-  Container,
-  Row,
-  Col,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+
+const API = "http://127.0.0.1:5000";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+	
     this.state = {
       hits: [],
       i: 1,
       maxI: 0,
-      text: "Nastepna strona"
+      text: "Nastepna strona",
+	  
+	  patientData: [],
+	  renP : 0
     };
+	
   }
+	
 
   onClickHandler() {
     console.log(this.state.i + " " + this.state.maxI);
@@ -40,8 +29,9 @@ class App extends React.Component {
       });
     } else {
       this.setState({
-        i: 1,
+        i:1,
         text: "Nastepna strona"
+
       });
     }
     if (this.state.i == this.state.maxI - 1) {
@@ -49,7 +39,7 @@ class App extends React.Component {
         text: "Pierwsza strona"
       });
     }
-    fetch("http://127.0.0.1:5000/list/" + this.state.i.toString())
+    fetch(API+"/list/" + this.state.i.toString())
       .then(response => response.json())
       .then(hits => {
         this.setState({
@@ -78,7 +68,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://127.0.0.1:5000/list/0")
+    fetch(API+"/list/0")
       .then(response => response.json())
       .then(hits => {
         this.setState({
@@ -87,39 +77,99 @@ class App extends React.Component {
         this.removeItem(0);
       });
   }
-  alertClicked() {
-    alert('You clicked the third ListGroupItem');
-}
 
+  click2(id){
+	  fetch(API+"/patient/"+id)
+      .then(response => response.json())
+      .then(patientData => {
+        this.setState({
+          patientData
+        })
+		this.setState({renP:1})
+      })
+	  this.render()
+  }
+  
+  onClick3(){
+	  this.setState({renP:0})
+	  this.render()
+  }
+  
+  patientComponent(){
+	  return (
+		<div>
+		  <h1> Informacje pacjenta </h1>
+		  <div>
+			<p>Imię: {this.state.patientData.patient.name[0].given[0]} </p>
+			<p>Nazwisko: {this.state.patientData.patient.name[0].family} </p>
+			<p>Płeć podczas narodzin/obecnie: {this.state.patientData.patient.extension[4].valueCode=='F'?"Kobieta":"Mężczyzna"}/
+												{this.state.patientData.patient.gender=='female'?"Kobieta":"Mężczyzna"} </p>
+			<div>
+				<h2> Dane o urodzeniu </h2>
+				<p>Data urodzenia: {this.state.patientData.patient.birthDate} </p>
+				<p>Miejsce urodzenia: {this.state.patientData.patient.extension[2].valueAddress.city+' '+
+										this.state.patientData.patient.extension[2].valueAddress.country+' '+
+										this.state.patientData.patient.extension[2].valueAddress.state} </p>
+				<p>Czy ma rodzeństwo: {this.state.patientData.patient.multipleBirthBoolean==true?'Tak':'Nie'} </p>
+				<div>
+					<h3> Rodzice</h3>
+					<p>Matka: {this.state.patientData.patient.extension[3].valueString} </p>
+					<p>Ojciec: {this.state.patientData.patient.extension[7].valueHumanName.text} </p>
+				</div>
+				<p>Rasa, Grupa etniczna: {this.state.patientData.patient.extension[0].valueCodeableConcept.coding[0].display}, 
+								{this.state.patientData.patient.extension[1].valueCodeableConcept.coding[0].display} </p>
+			</div>
+			
+			<p>Język: {this.state.patientData.patient.communication[0].language.coding[0].display} </p>
+			<p>Numer ubezpieczenia: {this.state.patientData.patient.extension[8].valueString} </p>
+			<p>Lekarz rodzinny: {this.state.patientData.patient.generalPractitioner[0].reference} </p>
+			<p>Czy wymagana opieka: {this.state.patientData.patient.extension[5].valueBoolean==true?'Tak':'Nie'} </p>
+			<p>Stan cywilny: {this.state.patientData.patient.maritalStatus.text} </p>
+			
+			<div>
+				<h2>Dane adresowe</h2>
+				<p>Kraj: {this.state.patientData.patient.address[0].country} </p>
+				<p>Stan: {this.state.patientData.patient.address[0].state} </p>
+				<p>Miasto: {this.state.patientData.patient.address[0].city} </p>
+				<p>Kod pocztowy: {this.state.patientData.patient.address[0].postalCode} </p>
+				<p>Adres: {this.state.patientData.patient.address[0].line[0]} </p>
+				<p>Adres2: {this.state.patientData.patient.address[0].line[1]=== undefined?"- - -":this.state.patientData.patient.address[0].line[1]} </p>
+				<p>Telefon: {this.state.patientData.patient.telecom[0].value} </p>
+				
+			</div>
+			
+			<p>Czy osoba fikcyjna: {this.state.patientData.patient.extension[6].valueBoolean==true?'Tak':'Nie'} </p>
+			<p>Data śmierci: {this.state.patientData.patient.deceasedDateTime=== undefined?"- - -":this.state.patientData.patient.deceasedDateTime} </p>
+			<p>Ostatnia aktualizacja: {this.state.patientData.patient.meta.lastUpdated} </p>
+		  </div>
+		  <button onClick={this.onClick3.bind(this)}>
+			  Wróć
+			</button>
+		 </div>
+		)
+  }
+  
+  listComponent(){
+	return (
+		  <div>
+			<ul>
+			  {this.state.hits.map(hit => <button key={hit.id} onClick = { this.click2.bind(this,hit.id) }> {hit.name} </button>)}
+			</ul>
+			<button onClick={this.onClickHandler.bind(this)}>
+			  {this.state.text}
+			</button>
+		  </div>
+		)
+  }
+  
+  
   render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="lg">
-          <NavbarBrand href="/">medigunwo</NavbarBrand>
-        </Navbar>
-        <Container>
-          <Row />
-          <Row>
-            <Col>Menumenu</Col>
-          </Row>
-          <Row>
-            <Col sm="12" md={{size: 8, offset: 2}}>
-              <ListGroup>
-                {this.state.hits.map(hit => (
-                  <ListGroupItem tag="a" action key={hit.id} onClick={this.alertClicked.bind(this)}> {hit.name} </ListGroupItem>
-                ))}
-              </ListGroup>
-              <Button color="danger" onClick={this.onClickHandler.bind(this)}>
-                {this.state.text}
-              </Button>
-            </Col>
-          </Row>
-          <Row>
-            <Col>Piekny koniec</Col>
-          </Row>
-        </Container>
-      </div>
-    );
+	if(this.state.renP==0){
+		return this.listComponent()
+	}
+	else {
+		return this.patientComponent()
+	}
   }
 }
 export default App;
